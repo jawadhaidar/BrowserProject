d806ae5d55cd83c427f147389a2d090ae29a42d2
jawad <jsh19@mail.aub.edu>
2023-11-25 09:23:56 -0800

    helper blue print

---
+++ helper_functions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 09:17:59 2023
+
+@author: mcc
+"""
+
+class Browser():
+    def __init__(self,database,order_list):
+        self.database=database 
+        self.order_list=order_list
+        
+    def openTab(self):
+        pass
+    def closeTab(self):
+        pass
+    def switchTab(self):
+        pass
+    def openNestedTab(self):
+        pass
+    def sortAllTabs(self):
+        pass
+    def saveTabs(self):
+        pass
+    def importTabs(self):
+        pass
+    def exitBrowser(self):
+        pass
+        
+    
+if __name__ == '__main__':
+    "test areana"
+    pass
